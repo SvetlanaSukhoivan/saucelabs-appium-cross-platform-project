c671f901ba4fc03177035fc35c6d2f3ea5bb1533
Svetlana Sukhoivan <svetlana.sukhoivan.mackbook@examplemail.com>
2026-03-26 14:26:54 +0200

    fix: update avd-name parameter and increase wait timeout

--- src/test/java/pages/BasePage.java
+++ src/test/java/pages/BasePage.java
@@ -16,7 +16,7 @@ public class BasePage {
 
     public BasePage(AppiumDriver driver) {
         this.driver = driver;
-        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(30));
 
         PageFactory.initElements(new AppiumFieldDecorator(driver, Duration.ofSeconds(10)), this);
     }
